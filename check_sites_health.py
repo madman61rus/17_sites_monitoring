268843a065af1311ad5909adc9a4a429c3a25d1d
madman61rus <rhjnhjtn3yjhe>
2017-01-11 00:25:11 +0300

    - Исправил нарушения PEP8 -+ заменил domain_name.split('//')[1] на встроенную функцию urlparse -+ заменил многословный лишний кусок [line for line in file.read().splitlines()] на простое чтение из файла сразу в список - Убрал вывод в консоль в функции load_urls4check. Она и так возвращает None в случае неудаче. При выводе есть проверка

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,13 +2,14 @@ import argparse
 import requests
 import datetime
 import whois
+from urllib.parse import urlparse
+
 
 def load_urls4check(path):
     try:
         with open(path) as file:
-            return [line for line in file.read().splitlines()]
+            return file.read().splitlines()
     except IOError:
-        print ('Не могу прочитать файл')
         return None
 
 
@@ -22,33 +23,33 @@ def is_server_respond_with_200(url):
     except requests.exceptions.ConnectionError:
         return None
 
+
 def get_domain_expiration_date(domain_name):
-    who =  whois.query(domain_name.split('//')[1])
+    who = whois.query(urlparse(domain_name).netloc)
     return who.expiration_date
 
-def print_results(url, respond_200,expiration):
-    if respond_200 == None:
+
+def print_results(url, respond_200, expiration):
+    if respond_200 is None:
         return None
 
     if respond_200 and expiration > 31:
-        print ('OK  Ответ от сервера {} - 200, количество оставшихся дней до окончания делегирования - {}'
-               .format(url, expiration))
+        message = 'OK Ответ от сервера {} - 200, кол-во оставшихся дней - {}'
+        print(message.format(url, expiration))
     else:
-        print ('Внимание! Ответ от сервера {} - {}, количество оставшихся дней до окончания делегирования - {}'
-               .format(url, respond_200, expiration))
+        message = 'Внимание! Ответ от сервера {} не 200, кол-во оставшихся дней - {}'
+        print(message.format(url, expiration))
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('-f','--file', required=True)
+    args.add_argument('-f', '--file', required=True)
     arguments = args.parse_args()
-    urls=load_urls4check(arguments.file)
+    urls = load_urls4check(arguments.file)
     for url in urls:
         response = is_server_respond_with_200(url)
         if response is None:
             print('Внимание ! Домен {} не найден'.format(url))
         else:
-            days =  get_domain_expiration_date(url) - datetime.datetime.now()
-            print_results(url,response , days.days)
-
-
-
+            days = get_domain_expiration_date(url) - datetime.datetime.now()
+            print_results(url, response, days.days)
